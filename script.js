d1a49335479aedd72ba9836c57cad280539283ef
Zach_Gilbert <zach.gilbert2020@gmail.com>
2023-11-08 15:13:29 -0500

    Update

--- script.js
+++ script.js
@@ -3,53 +3,78 @@ let minRow = 0;
 let maxCol = 0;
 let maxRow = 0;
 
-document.querySelector("form").addEventListener("submit", function (e) {
-  e.preventDefault();
 
+jQuery.validator.setDefaults ({
+    debug:true,
+    success:"valid"
 
-    minCol = document.getElementById("minCol").value;
-    maxCol = document.getElementById("maxCol").value;
-    minRow = document.getElementById("minRow").value;
-    maxRow = document.getElementById("maxRow").value;
-
-    var errorDiv = document.getElementById("inputError");
-    if((maxCol > 50 || minCol < -50) || (maxRow > 50 || minRow < -50)) {
-      //Show Error
-      errorDiv.innerText = "An Input is Out of Range!!!! (-50 to 50)";
-      //alert("Temp Error, an input is out of range");
-    } else if((minCol > maxCol) || (minRow > maxRow)) {
-      //throw Error
-      errorDiv.innerText = "A Min Value CANNOT Exceed a Max Value!";
-      //alert("Temp Error, an minimum cannot be greater or equal to max");
-    } else {
-      errorDiv.innerText = "";
-      const table = document.createElement("table");
-      const tabledata = document.getElementById("table");
-      tabledata.innerHTML = "";
-      minRow -= 1;
-      minCol -= 1;
-      for (let i = minRow; i <= maxRow; i++) {
-        row = document.createElement("tr");
-        for (let j = minCol; j <= maxCol; j++) {
-            colH = document.createElement("th");
-            if(i == minRow) {
-              if(i == minRow && j == minCol){
-                colH.innerText = " ";
-                row.appendChild(colH);
-              } else {
-                colH.innerText = j;
-                row.appendChild(colH);
-              }
-            } else if(j == minCol) {
-              colH.innerText = i;
+});
+$("#form").validate({
+  rules: {
+    minCol: {
+        required: true,
+        integer: true,
+        range: [-50, 50]
+    },
+    maxCol: {
+      required: true,
+      integer: true,
+      range: [-50, 50]
+    },
+    minRow: {
+      required: true,
+      integer: true,
+      range: [-50, 50]
+    },
+    maxRow: {
+      required: true,
+      integer: true,
+      range: [-50, 50]
+    }, 
+  },
+  messages: {
+    minCol: {
+        required: "Please Enter a Minimum Column",
+        integer: "Please Enter an Integer",
+        range: "Please Stay Between the Ranges of (-50 to 50)"
+    }
+  },  
+  submitHandler: function(form) {
+  // do other things for a valid form
+  minCol = document.getElementById("minCol").value;
+  maxCol = document.getElementById("maxCol").value;
+  minRow = document.getElementById("minRow").value;
+  maxRow = document.getElementById("maxRow").value;
+    
+  const table = document.createElement("table");
+    const tabledata = document.getElementById("table");
+    tabledata.innerHTML = "";
+    minRow -= 1;
+    minCol -= 1;
+    for (let i = minRow; i <= maxRow; i++) {
+      row = document.createElement("tr");
+      for (let j = minCol; j <= maxCol; j++) {
+          colH = document.createElement("th");
+          if(i == minRow) {
+            if(i == minRow && j == minCol){
+              colH.innerText = " ";
               row.appendChild(colH);
-            }else {
-              colH.innerText = j * i;
+            } else {
+              colH.innerText = j;
               row.appendChild(colH);
             }
-        }
-        table.appendChild(row);
+          } else if(j == minCol) {
+            colH.innerText = i;
+            row.appendChild(colH);
+          }else {
+            colH.innerText = j * i;
+            row.appendChild(colH);
+          }
       }
-      tabledata.appendChild(table);
-      }
+      table.appendChild(row);
+    }
+    tabledata.appendChild(table);
+  form.submit();
+  }
 });
+
